eaa76a73a4d5bb1eb9e4440a613d3b888a4f615b
Ricardo Hatsugai <rhatsugai@gmail.com>
2022-07-24 09:00:03 -0300

    Exercícios de quociente inteiro e cálculo com casas de decimais

---
+++ exercicios/variaveis/calculoComCasasDecimais.py
@@ -0,0 +1,21 @@
+'''
+    Este é um velho dilema principalmente de linguagens antigas.
+    A precisão de casas decimais nos cálculos monetários com mais de duas caasas decimais.
+    Uma solução simples, sem utilizar o recurso que o Python o proporciona, é:
+    -> multiplicação por 1.000 -> decimalParaInteiro ->  cálculo -> inteiroParaDecimal ->
+    divisão por 1.000.000, assim se obtem o resultado com precisão de casas de cimais.
+'''
+
+quantFloat = float(input("Digite a quantidade: "))
+
+valorFloat = float(input("Digite o valor: "))
+
+quantInteiro = int(quantFloat * 1000)
+valorInteiro = int(valorFloat * 1000)
+
+totalInteiro = quantInteiro * valorInteiro
+
+totalFloat = float(totalInteiro)
+totalFloat = totalFloat / 1000000
+
+print("O valor total é: ", totalFloat)
---
+++ exercicios/variaveis/quocienteInteiro.py
@@ -0,0 +1,11 @@
+'''
+    Obtenha o quociente inteiro da divisão 5 por 3, o seja, sem
+    as casas decimais.
+    "//" efetura a divisão inteira, sem as cadas decimais.
+'''
+
+dividendo = 5.0
+divisor = 3.0
+resultado = dividendo // divisor
+
+print(resultado)
