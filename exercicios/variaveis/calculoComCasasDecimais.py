139b6c7f71b13df848b581876df5af103dd476d6
Ricardo Hatsugai <rhatsugai@gmail.com>
2022-07-25 02:06:42 -0300

    Calculo com casas de decimais

--- exercicios/variaveis/calculoComCasasDecimais.py
+++ exercicios/variaveis/calculoComCasasDecimais.py
@@ -1,9 +1,10 @@
 '''
     Este é um velho dilema principalmente de linguagens antigas.
-    A precisão de casas decimais nos cálculos monetários com mais de duas caasas decimais.
+    A precisão de casas decimais nos cálculos monetários com mais de duas casas decimais.
     Uma solução simples, sem utilizar o recurso que o Python o proporciona, é:
     -> multiplicação por 1.000 -> decimalParaInteiro ->  cálculo -> inteiroParaDecimal ->
-    divisão por 1.000.000, assim se obtem o resultado com precisão de casas de cimais.
+    divisão por 1.000.000 (pois 1.000 x 1.000 dá 1.000.000), assim se obtêm o resultado com
+     precisão de casas decimais.
 '''
 
 quantFloat = float(input("Digite a quantidade: "))
@@ -16,6 +17,7 @@ valorInteiro = int(valorFloat * 1000)
 totalInteiro = quantInteiro * valorInteiro
 
 totalFloat = float(totalInteiro)
+
 totalFloat = totalFloat / 1000000
 
 print("O valor total é: ", totalFloat)
