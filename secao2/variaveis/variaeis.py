b14012d483a1c4ab1aca3881ee66e5ded56ba82c
Ricardo Hatsugai <rhatsugai@gmail.com>
2023-10-15 23:51:20 -0300

    Tipos de variáveis

--- secao2/variaveis/variaeis.py
+++ secao2/variaveis/variaeis.py
@@ -1,1 +1,4 @@
-print(10**2)
+quantidade = float(input("Digite um número decimal: "))
+casa_decimal = int(input("Digite a quantidade de casas decimais que deseja: "))
+
+print('A quantidade de itens é {:.4f}' .format(quantidade))
